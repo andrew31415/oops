3dbdf3692c473246abba1531bd3943895f586616
Andrei <andrei.sandu91@gmail.com>
2023-07-15 18:53:28 +0300

    Add Stack
    How:
    
    * Fix VSCode crashing tests.
    * Added Stack.
    * Reworked SLL.add_node() to include a flag that
      iterates over the value given instead of giving
      the user 2 different methods.
    * TDD for Stack.
    * Docstring updates.
    
    Issue: #14

--- tests/test_lists.py
+++ tests/test_lists.py
@@ -129,15 +129,86 @@ class TestSinglyLinkedList:
         with pytest.raises(ValueError):
             sll_simple[2.5]
 
-    # Check if can make a list from a non-iterable type
-    sll = lists.SinglyLinkedList(2)
-    assert sll.length == 1
+        # Add an element with val=[1,2,3]
+        sll.add([1, 2, 3])
+        assert len(sll) == 3
 
-    # Check if `None` value can be inserted correctly
-    sll = lists.SinglyLinkedList()
-    assert sll.length == 0
-    sll.add_node(None)
-    assert sll.length == 1
+        # Check if can make a list from a non-iterable type
+        sll = lists.SinglyLinkedList(2)
+        assert len(sll) == 1
+
+        # Check if `None` value can be inserted correctly
+        sll = lists.SinglyLinkedList()
+        assert len(sll) == 0
+        sll.add(None)
+        assert len(sll) == 1
+
+    def test_can_add_multiple_nodes_to_sll(self, sll_simple, sll_complex):
+        """Add node(s) to a singly linked list.
+        """
+        assert sll_simple._size == SIMPLE_LIST_LEN
+
+        sll_simple.add([5, 6, 7], iterable=True)
+        assert sll_simple._size == (SIMPLE_LIST_LEN + 3)
+
+        sll_simple.add('test', iterable=True)
+        assert sll_simple._size == (SIMPLE_LIST_LEN + 3 + 4)
+
+        sll_simple.add(sll_complex, iterable=True)
+        assert sll_simple._size == (
+            SIMPLE_LIST_LEN + 3 + 4 + COMPLEX_LIST_LEN)
+        sll = lists.SinglyLinkedList()
+        sll.add("haha", iterable=True)
+        assert len(sll) == 4
+
+    def test_can_create_new_sll_from_different_types(self, sll_simple):
+        """Create singly linked lists from different types of input variables.
+        """
+        sll = lists.SinglyLinkedList(SIMPLE_LIST)
+
+        assert sll == sll_simple
+
+        sll = lists.SinglyLinkedList(SIMPLE_SET)
+        assert len(sll) == 3
+
+    def test_iterable_sll(self, sll_simple):
+        """Check if the SLL is interable
+        """
+        sll_iter = iter(sll_simple)
+        assert next(sll_iter) == lists.ListNode(val=1, next=lists.ListNode(
+            val=2, next=lists.ListNode(val=3, next=lists.ListNode(val=1))))
+        assert next(sll_iter) == lists.ListNode(val=2, next=lists.ListNode(val=3, next=lists.ListNode(val=1)))
+        assert next(sll_iter) == lists.ListNode(val=3, next=lists.ListNode(val=1))
+        assert next(sll_iter) == lists.ListNode(val=1)
+
+        with pytest.raises(StopIteration):
+            assert next(sll_iter)
+
+    def test_raise_errors(self, sll_simple):
+        with pytest.raises(TypeError):
+            sll_simple.add(10, iterable=True)
+
+        # sll = lists.SinglyLinkedList()
+        # with pytest.raises(ValueError):
+        #     sll.length = -10
+
+    def test_sll_len_function(self, sll_simple, sll_complex):
+        assert len(sll_simple) == 4
+        assert len(sll_complex) == 3
+
+    def test_sll_getitem_function(self, sll_simple):
+        assert sll_simple[0] == lists.ListNode(val=1, next=lists.ListNode(
+            val=2, next=lists.ListNode(val=3, next=lists.ListNode(val=1))))
+        assert sll_simple[1] == lists.ListNode(val=2, next=lists.ListNode(val=3, next=lists.ListNode(val=1)))
+        assert sll_simple[2] == lists.ListNode(val=3, next=lists.ListNode(val=1))
+        assert sll_simple[3] == lists.ListNode(val=1)
+
+        with pytest.raises(IndexError):
+            sll_simple[4]
+        with pytest.raises(ValueError):
+            sll_simple[-10]
+        with pytest.raises(ValueError):
+            sll_simple[2.5]
 
 
 class TestStack:
@@ -191,6 +262,9 @@ class TestStack:
         with pytest.raises(IndexError):
             stack.top()
 
+        # Check if can make a list from a non-iterable type
+        stack = lists.SinglyLinkedStack(2)
+        assert len(stack) == 1
 
 class TestQueue:
 
@@ -252,6 +326,8 @@ class TestQueue:
         with pytest.raises(TypeError):
             queue.enqueue(1, iterate=True)
 
+    def test_pop_and_top(self):
+        stack = lists.SinglyLinkedStack([1, 2, 3])
 
 class TestCircularyLinkedList:
     def test_create(self):
